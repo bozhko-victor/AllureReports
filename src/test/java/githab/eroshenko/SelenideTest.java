ebd1ab094e7a161d57e87c523d55e4ad21149c97
Victor <bozhko.v.n@yandex.ru>
2021-09-15 20:44:21 +0300

    Added SelenideTest & modified build.gradle

---
+++ src/test/java/githab/eroshenko/SelenideTest.java
@@ -0,0 +1,26 @@
+package githab.eroshenko;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class SelenideTest {
+
+    @Test
+    public void testGithab() {
+        open("https://github.com");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $(partialLinkText("Issues")).click();
+        $(byText("68")).shouldBe(Condition.visible);
+    }
+}
