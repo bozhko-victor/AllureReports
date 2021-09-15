f0a966a5289c2dbb84dac95cf724fbf481fbf534
Victor <bozhko.v.n@yandex.ru>
2021-09-15 23:33:04 +0300

    Modified AnnotetedStepTest & added WebSteps

---
+++ src/test/java/githab/eroshenko/WebSteps.java
@@ -0,0 +1,43 @@
+package githab.eroshenko;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+import static githab.eroshenko.AnnotetedStepTest.ISSUE_NUMBER;
+import static githab.eroshenko.AnnotetedStepTest.REPOSITORY;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class WebSteps {
+
+    @Step("Открываем главную страницу")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Ищем репозиторий {repository}")
+    public void searchRepository(String repository) {
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(REPOSITORY);
+        $(".header-search-input").submit();
+    }
+
+    @Step("Переходим в репозиторий {repository}")
+    public void goToRepository(String repository) {
+        $(linkText(REPOSITORY)).click();
+    }
+
+    @Step("Открываем раздел Issues")
+    public void openIssueTab() {
+        $(partialLinkText("Issues")).click();
+    }
+
+    @Step("Проверяем наличие Issue с номером {number}")
+    public void shouldSeeIssueWithNumber(int number) {
+        $(byText("" + ISSUE_NUMBER)).should(Condition.visible);
+    }
+}
